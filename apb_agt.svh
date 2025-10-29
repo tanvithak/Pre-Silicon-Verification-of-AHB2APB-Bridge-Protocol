8344130b186acd8d4207942a699705213f6597dc
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-10-29 13:46:35 -0700

    Enhance apb_agt with bridge configuration support
    Added bridge configuration handling to apb_agt class.

--- apb_agt.svh
+++ apb_agt.svh
@@ -1,9 +1,3 @@
-`include "uvm_macros.svh" 
-`include "apb_sequencer.svh"
-`include "apb_driver.svh"
-`include "apb_monitor.svh"
-import uvm_pkg::*;
-
 class apb_agt extends uvm_agent;
  `uvm_component_utils(apb_agt)
 
@@ -11,19 +5,29 @@ class apb_agt extends uvm_agent;
   super.new(name,parent);
  endfunction
 
+ bridge_cfg ahb_apb_cfg;
  apb_sequencer apb_seqr;
  apb_driver apb_drv;
  apb_monitor apb_mon;
 
  virtual function void build_phase(uvm_phase phase);
+  if(!uvm_config_db #(bridge_cfg)::get(this,"",bridge_cfg,ahb_apb_cfg)
+   `uvm_fatal(get_type_name(),"bridge_cfg get operation failed")
+
   super.build_phase(phase);
+
   apb_mon = apb_monitor::type_id::create("apb_mon",this);
-  apb_seqr = apb_sequencer::type_id::create("apb_seqr",this);
-  apb_drv = apb_driver::type_id::create("apb_drv",this);
+
+  if(ahb_apb_cfg.apb_is_active)
+   begin
+    apb_seqr = apb_sequencer::type_id::create("apb_seqr",this);
+    apb_drv = apb_driver::type_id::create("apb_drv",this);
+   end
  endfunction
 
  virtual function void connect_phase(uvm_phase phase);
-  apb_drv.seq_item_port.connect(apb_seqr.seq_item_export);
+  if(ahb_apb_cfg.apb_is_active)
+   apb_drv.seq_item_port.connect(apb_seqr.seq_item_export);
  endfunction
 
 endclass
