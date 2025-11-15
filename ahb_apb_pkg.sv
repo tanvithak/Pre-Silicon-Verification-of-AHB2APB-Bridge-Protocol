cad3962cd520d786c74bcb6740e51c7b1134f6d4
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-11-14 16:51:12 -0800

    Refactor bridge_pkg to include new files
    Added additional includes for bridge configuration and virtual components.

--- ahb_apb_pkg.sv
+++ ahb_apb_pkg.sv
@@ -2,6 +2,7 @@ package bridge_pkg;
  `include "uvm_macros.svh" 
  import uvm_pkg::*;
 
+ `include "bridge_cfg.svh"
  `include "ahb_seq_item.svh"
  `include "ahb_monitor.svh"
  `include "ahb_sequencer.svh"
@@ -16,6 +17,10 @@ package bridge_pkg;
  `include "apb_driver.svh"
  `include "apb_agt.svh"
 
+ `include "scoreboard.svh"
+ `include "virtual_sequencer.svh"
+ `include "virtual_sequence.svh"
  `include "envir.svh"
  `include "test.svh"
+
 endpackage
