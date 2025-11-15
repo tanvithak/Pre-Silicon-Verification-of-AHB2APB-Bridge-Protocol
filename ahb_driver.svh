6b38dabfbd1fb9b9ed0a374c6f58aa261ca7a115
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-11-14 16:51:36 -0800

    Refactor ahb_driver for interface consistency

--- ahb_driver.svh
+++ ahb_driver.svh
@@ -1,6 +1,3 @@
-`include "uvm_macros.svh" 
-`include "ahb_seq_item.svh"
-import uvm_pkg::*;
 class ahb_driver extends uvm_driver#(ahb_seq_item);
  `uvm_component_utils(ahb_driver)
  virtual ahb_intf ahb_vif;
@@ -13,17 +10,17 @@ class ahb_driver extends uvm_driver#(ahb_seq_item);
 
  virtual function void build_phase(uvm_phase phase);
   super.build_phase(phase);
-  if(!uvm_config_db#(virtual ahb_intf)::get(this,"","ahb_vif",ahb_vif))
+  if(!uvm_config_db#(virtual ahb_intf)::get(this,"","ahb_intf",ahb_vif))
    `uvm_fatal(get_full_name(), "Getting virtual interface from the configuration failed")
  endfunction
 
  virtual task run_phase(uvm_phase phase);
   req = ahb_seq_item::type_id::create("req");
-  @(ahb_vif.ahb_drv_cb);
-  ahb_vif.ahb_drv_cb.HRESETn <= 1'b0;
+  @(ahb_vif.AHB_DRV_CB);
+  ahb_vif.AHB_DRV_CB.HRESETn <= 1'b0;
 
-  @(ahb_vif.ahb_drv_cb);
-  ahb_vif.ahb_drv_cb.HRESETn <= 1'b1;
+  @(ahb_vif.AHB_DRV_CB);
+  ahb_vif.AHB_DRV_CB.HRESETn <= 1'b1;
 
   forever
    begin
@@ -37,24 +34,24 @@ class ahb_driver extends uvm_driver#(ahb_seq_item);
 
  task send_to_dut(ahb_seq_item xtn);
 
-    ahb_vif.AHB_DRV_MP.ahb_drv_cb.HADDR <= xtn.haddr;
-    ahb_vif.AHB_DRV_MP.ahb_drv_cb.HTRANS <= xtn.htrans;
-    ahb_vif.AHB_DRV_MP.ahb_drv_cb.HSIZE <= xtn.hsize; 
-    ahb_vif.AHB_DRV_MP.ahb_drv_cb.HWRITE <= xtn.hwrite;
-    ahb_vif.AHB_DRV_MP.ahb_drv_cb.HREADY_IN <= 1;
+    ahb_vif.AHB_DRV_MP.AHB_DRV_CB.HADDR <= xtn.haddr;
+    ahb_vif.AHB_DRV_MP.AHB_DRV_CB.HTRANS <= xtn.htrans;
+    ahb_vif.AHB_DRV_MP.AHB_DRV_CB.HSIZE <= xtn.hsize; 
+    ahb_vif.AHB_DRV_MP.AHB_DRV_CB.HWRITE <= xtn.hwrite;
+    ahb_vif.AHB_DRV_MP.AHB_DRV_CB.HREADY_IN <= 1;
 
-  @(posedge ahb_vif.AHB_DRV_MP.ahb_drv_cb) 
+  @(posedge ahb_vif.AHB_DRV_MP.AHB_DRV_CB) 
    wait (ahb_vif.HREADY_OUT);
  
   if(xtn.hwrite)
    begin
     //write operation 
-    ahb_vif.AHB_DRV_MP.ahb_drv_cb.HWDATA <= xtn.hwdata; 
+    ahb_vif.AHB_DRV_MP.AHB_DRV_CB.HWDATA <= xtn.hwdata; 
    end
   else
    begin
     //ready operation
-    ahb_vif.AHB_DRV_MP.ahb_drv_cb.HWDATA <= 'x;  
+    ahb_vif.AHB_DRV_MP.AHB_DRV_CB.HWDATA <= 'x;  
    end
 
  endtask
