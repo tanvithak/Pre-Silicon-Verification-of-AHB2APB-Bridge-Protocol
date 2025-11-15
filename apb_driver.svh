68be214f2545e67cf6ca2d0b55900a534fd00c95
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-11-14 16:55:28 -0800

    Refactor apb_driver class and fix interface references

--- apb_driver.svh
+++ apb_driver.svh
@@ -1,6 +1,3 @@
-`include "uvm_macros.svh" 
-`include "apb_seq_item.svh"
-import uvm_pkg::*;
 class apb_driver extends uvm_driver#(apb_seq_item);
  `uvm_component_utils(apb_driver)
  virtual apb_intf apb_vif;
@@ -13,7 +10,7 @@ class apb_driver extends uvm_driver#(apb_seq_item);
 
  virtual function void build_phase(uvm_phase phase);
   super.build_phase(phase);
-  if(!uvm_config_db#(virtual apb_intf)::get(this,"","apb_vif",apb_vif))
+  if(!uvm_config_db#(virtual apb_intf)::get(this,"","apb_intf",apb_vif))
    `uvm_fatal(get_full_name(), "Getting virtual interface from the configuration failed")
  endfunction
 
@@ -29,16 +26,16 @@ class apb_driver extends uvm_driver#(apb_seq_item);
  endtask
 
  task send_to_dut(apb_seq_item apb_xtn);
-  apb_vif.APB_DRV_MP.apb_drv_cb.PRESETn <= apb_xtn.presetn;
-  apb_vif.APB_DRV_MP.apb_drv_cb.PENABLE <= apb_xtn.penable;
-  apb_vif.APB_DRV_MP.apb_drv_cb.PSEL <= apb_xtn.psel;
-  apb_vif.APB_DRV_MP.apb_drv_cb.PWRITE <= apb_xtn.pwrite;
-  apb_vif.APB_DRV_MP.apb_drv_cb.PADDR <= apb_xtn.paddr;
+  apb_vif.APB_DRV_MP.APB_DRV_CB.PRESETn <= apb_xtn.presetn;
+  apb_vif.APB_DRV_MP.APB_DRV_CB.PENABLE <= apb_xtn.penable;
+  apb_vif.APB_DRV_MP.APB_DRV_CB.PSEL <= apb_xtn.psel;
+  apb_vif.APB_DRV_MP.APB_DRV_CB.PWRITE <= apb_xtn.pwrite;
+  apb_vif.APB_DRV_MP.APB_DRV_CB.PADDR <= apb_xtn.paddr;
 
   if(apb_xtn.pwrite)
-   apb_vif.APB_DRV_MP.apb_drv_cb.PWDATA <= apb_xtn.pwdata;
+   apb_vif.APB_DRV_MP.APB_DRV_CB.PWDATA <= apb_xtn.pwdata;
   else
-   apb_vif.APB_DRV_MP.apb_drv_cb.PWDATA <= 'x;
+   apb_vif.APB_DRV_MP.APB_DRV_CB.PWDATA <= 'x;
  endtask
 
 
