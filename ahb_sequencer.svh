6e2f7467133b09c9b51940e4e172d01cdd5fd59f
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-11-29 16:22:53 -0800

    Update and rename ahb_sequencer.svh to ahb_sequencer.sv

--- ahb_sequencer.svh
+++
@@ -1,8 +0,0 @@
-class ahb_sequencer extends uvm_sequencer#(ahb_seq_item);
- `uvm_component_utils(ahb_sequencer)
- 
- function new(string name="ahb_sequencer",uvm_component parent);
-  super.new(name,parent);
- endfunction
-
-endclass
