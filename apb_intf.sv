17b58b8cdca6d23671d2d34a3c384da058da42cf
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-11-29 16:26:09 -0800

    Refactor apb_intf interface and clocking blocks

--- apb_intf.sv
+++ apb_intf.sv
@@ -11,12 +11,8 @@ interface apb_intf(input bit CLK);
 
  clocking APB_DRV_CB@(posedge CLK);
   default input #1 output #1;
-  output PRESETn;
-  output PENABLE;
-  output PSEL;
-  output PWRITE;
-  output PADDR;
-  output PWDATA;
+  output PRDATA;
+  output PREADY;
  endclocking
 
  clocking APB_MON_CB@(posedge CLK);
